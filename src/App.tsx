5b35f6cf2fd34451a36a0004a4dc4c7384e6dd47
mehul455 <gmehul127@gmail.com>
2023-11-02 15:14:32 +0530

    without builder commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ import MainC from './pages/MainC'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainC />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/setting" element={<Setting />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/:slug" element={<MainC/>} />
+      {/* <Route path="/:slug" element={<MainC/>} /> */}
     </Routes>
   )
 }
